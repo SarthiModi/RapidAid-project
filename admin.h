5c73a8420fe0de1471789b1e49ace7b5f6b58c92
SarthiModi <sarthimodi333@gmail.com>
2026-03-11 21:39:30 +0530

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,6 +1,21 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-int adminLogin();
-void adminMenu();
+/* admin operations */
+void admin_add_ambulance(const char *id, const char *loc);
+void admin_add_hospital(const char *id, const char *loc, int beds);
+void admin_update_ambulance_status(const char *id, const char *new_status);
+
+/* views */
+void admin_view_ambulances(void);
+void admin_view_hospitals(void);
+void admin_view_requests(void);
+
+/* advanced */
+void admin_approve_request(void);
+
+/* new DS features */
+void admin_find_ambulance(const char *id);    /* BST search */
+void admin_view_completed(void);             /* show completed stack */
+
 #endif

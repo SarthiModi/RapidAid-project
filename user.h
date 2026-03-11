3b92160cb7c4dcf4d2b93310a295dcd7f1461169
SarthiModi <sarthimodi333@gmail.com>
2026-03-11 21:44:49 +0530

    Update user.h

--- user.h
+++ user.h
@@ -1,33 +1,8 @@
 #ifndef USER_H
 #define USER_H
 
-#include "graph.h"     // For printGraph()
-#include "resource.h"  // For reportAccident()
-// ---------------- Constants ----------------
-#define MAX_USERS 10
-
-// ----------------- User Structures -----------------
-typedef struct {
-    char username[50];
-    char password[50];
-} User;
-
-// ----------------- Function Prototypes -----------------
-
-// Login / Sign-up page (first screen inside User Login)
-void userPage();
-
-// User menu after login
-void userMenu(int userIndex);
-
-
-// User sign-up
-void userSignUp();
-
-// User login validation
-int userLogin(int *userIndex);
-
-// Optional: initialize a demo user
-void initDemoUser();
+void user_report_accident(const char *userID, const char *location, const char *severity, const char *contact);
+void user_check_status(const char *userID); /* checks pending/assigned/completed */
+void user_confirm_arrival(const char *request_id_str); 
 
 #endif

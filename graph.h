74b989946b31fae769c69f8916edde50cbfd1ac8
SarthiModi <sarthimodi333@gmail.com>
2026-03-11 21:41:27 +0530

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,11 +1,34 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#define N 5
+#include <limits.h>
 
-void initGraph();
-void addRoad(int u, int v, int w);
-void buildCity();
-void printGraph();
+#define MAXN 300
+#define MAX_NAME 64
+#define MAX_PATH_LEN 1024
 
-#endif
+typedef struct Edge {
+    int to, weight;
+    struct Edge *next;
+} Edge;
+
+typedef struct {
+    char name[MAX_NAME];
+    Edge *adj;
+} Node;
+
+extern Node graph[MAXN];
+extern int nodeCount;
+
+/* node helpers */
+int getNodeIndex(const char *name);
+int findNodeIndex(const char *name);
+
+/* graph load */
+void addEdge(const char *a, const char *b, int w);
+void loadGraph(const char *filename);
+
+
+int dijkstra_with_path(const char *src, const char *dest, char *route_buf, int buf_len);
+
+#endif /* GRAPH_H */

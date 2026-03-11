98c9face464bef9f632e885e65cb00795907fb7f
SarthiModi <sarthimodi333@gmail.com>
2026-03-11 21:34:56 +0530

    Update admin.c

--- admin.c
+++ admin.c
@@ -9,14 +9,14 @@ int adminLogin() {
     printf("\n--- Admin Login ---\nUsername: "); scanf("%s", uname);
     printf("Password: "); scanf("%s", pass);
     if(strcmp(uname,"admin")==0 && strcmp(pass,"admin123")==0) {
-        printf("✅ Admin login successful!\n"); return 1;
+        printf("Admin login successful!\n"); return 1;
     }
-    printf("❌ Invalid admin credentials!\n"); return 0;
+    printf(" Invalid admin credentials!\n"); return 0;
 }
 
 void viewAccidentLog() {
     FILE *fp=fopen("accident_log.txt","r");
-    if(!fp){ printf("⚠️ No accident records found!\n"); return; }
+    if(!fp){ printf("No accident records found!\n"); return; }
     char line[200]; printf("\n--- Accident Logs ---\n");
     while(fgets(line,sizeof(line),fp)) printf("%s",line);
     fclose(fp); printf("-------------------\n");
@@ -24,14 +24,21 @@ void viewAccidentLog() {
 
 void adminMenu() {
     int choice;
+    int aid,aloc;
+    char aname[50];
     while(1){
-        printf("\n--- Admin Menu ---\n1.View Accident Logs\n2.Show Ambulances\n3.Show Hospitals\n4.Logout\nEnter choice: ");
+        printf("\n--- Admin Menu ---\n1.View Accident Logs\n2.Show Ambulances\n3.Show Hospitals\n4.To Add Ambulance\n5.Logout\nEnter choice: ");
         scanf("%d",&choice);
         switch(choice){
             case 1: viewAccidentLog(); break;
             case 2: printAmbulances(); break;
             case 3: printHospitals(); break;
-            case 4: printf("Logging out of admin...\n"); return;
+            case 4: printf("\nEnter the id:");scanf("%d",&aid);
+                    printf("\nEnter the location:");scanf("%d",&aloc);
+                    getchar();
+                    printf("\nEnter Operator name:");scanf("%s",aname);
+            addAmbulance(aid,aname,aloc);break;
+            case 5: printf("Logging out of admin...\n"); return;
             default: printf("Invalid choice!\n");
         }
     }
